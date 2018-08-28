0952df14188aff6175eefbcc87850f134d3dfcbf
MasonA <mason@ourprairie.net>
2018-08-28 10:32:04 -0700

    start splash page

---
+++ client/web/components/splash/splash.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Splash extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ client/web/components/splash/splash_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import Splash from './splash';
+
+const mapStateToProps = () => ({
+
+});
+
+const mapDispatchToProps = () => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Splash);

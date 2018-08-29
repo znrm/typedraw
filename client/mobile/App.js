9fea829239e128e689af53fb8c47aafaab0bd96b
matt_moe <mrobertmoe@gmail.com>
2018-08-29 11:09:12 -0700

    small change

--- client/mobile/App.js
+++ client/mobile/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>TypeDraw</Text>
+        <Text>TypeDraw!</Text>
       </View>
     );
   }

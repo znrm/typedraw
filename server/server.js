0b9f90ff1dc35764a6b5f10fd277e7f2aa415a35
znrm <moradianzaniar@gmail.com>
2018-08-29 20:07:48 -0700

    Write logic for sockets broadcasting to all

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const db = process.env.PROD_MONGODB || require('./config/keys').mongoURI;
 
 require('./config/passport')(passport);
 
-
 mongoose
   .connect(
     encodeURI(db),
@@ -51,8 +50,16 @@ const server = app.listen(port, () =>
 
 const io = socketIO.listen(server);
 
+let text = '';
+
 io.sockets.on('connection', socket => {
-  socket.on('typing', letter => console.log(letter));
+  socket.on('typing', letter => {
+    text += letter;
+    io.broadcast('receiveText', text);
+  });
+
+  socket.on('textLayer', socket.join('textRoom'));
+
   socket.on('working', res => {
     console.log(res);
     io.emit('greeting', 'welcome to typedraw, this was sent with a socket');

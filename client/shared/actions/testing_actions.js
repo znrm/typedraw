4bafe213c6d9d75d6a212453a7443c2bcec7e91c
znrm <moradianzaniar@gmail.com>
2018-08-29 21:21:08 -0700

    Commiting socket middleware for integration with document components

--- client/shared/actions/testing_actions.js
+++
@@ -1,6 +0,0 @@
-export const TYPING = 'TYPING';
-
-export const typing = key => ({
-  type: TYPING,
-  key
-});
--- client/shared/store/socket_middleware.js
+++ client/shared/store/socket_middleware.js
@@ -1,10 +1,10 @@
 import startSocket from '../util/socket_util';
-import { TYPING } from '../actions/testing_actions';
+import { RECEIVE_NEW_KEYS } from '../actions/document_actions';
 
 const socket = startSocket();
 
 const socketMiddleware = store => next => action => {
-  if (action.type === TYPING) {
+  if (action.type === RECEIVE_NEW_KEYS) {
     socket.emit('typing', action.key);
   } else {
     next(action);

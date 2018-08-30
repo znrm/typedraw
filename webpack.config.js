73d69ee3d80d1768616f088c8b90278a641a8533
znrm <moradianzaniar@gmail.com>
2018-08-29 18:24:43 -0700

    Upgrade babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,11 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           query: {
-            presets: ['env', 'react'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },

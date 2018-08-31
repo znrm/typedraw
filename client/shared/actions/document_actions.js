175d71aa8d1bfc3ac3024f9df65d19928e9359c8
MasonA <mason@ourprairie.net>
2018-08-30 21:23:29 -0700

    add document api calls

--- client/shared/actions/document_actions.js
+++ client/shared/actions/document_actions.js
@@ -1,5 +1,4 @@
-// import api stuff
-// import * as DocAPIUtil from '../somwhereTBD';
+import * as DocAPIUtil from '../util/document_api_util';
 
 export const UPDATE_TEXT = 'UPDATE_TEXT';
 export const UPDATE_IMAGE = 'UPDATE_IMAGE';
@@ -21,12 +20,12 @@ export const receiveKeys = (docId, keys) => ({
   keys
 });
 
-// export const updateText = (document) => dispatch => (
-//   DocAPIUtil.updateText(document)
-//     .then(servDoc => dispatch(onUpdateText(servDoc)), err => console.log(err, 'edit error'))
-// );
+export const updateText = (document) => dispatch => (
+  DocAPIUtil.updateText(document)
+    .then(servDoc => dispatch(onUpdateText(servDoc)), err => console.log(err, 'edit error'))
+);
 
-// export const updateImage = (image) => dispatch => (
-//   DocAPIUtil.updateImage(image)
-//     .then(servImg => dispatch(onUpdateImage(servImg)), err => console.log(err, 'pic error'))
-// );
+export const updateImage = (image) => dispatch => (
+  DocAPIUtil.updateImage(image)
+    .then(servImg => dispatch(onUpdateImage(servImg)), err => console.log(err, 'pic error'))
+);

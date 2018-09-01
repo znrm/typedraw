6b502852efea8a4d1f8e4c4dce2b08f215db2a5a
Aidan Gadberry <agadberr@gmail.com>
2018-08-31 22:16:14 -0700

    Add deletion to reducer actions

--- client/shared/reducers/documents_reducer.js
+++ client/shared/reducers/documents_reducer.js
@@ -1,7 +1,8 @@
 import { merge } from 'lodash';
 import {
   RECEIVE_DOCUMENT,
-  RECEIVE_DOCUMENT_DIFFS
+  RECEIVE_DOCUMENT_DIFFS,
+  REMOVE_DOCUMENT
 } from '../actions/document_actions';
 
 // This has not been implemented yet
@@ -24,18 +25,29 @@ const DocumentReducer = (state = initialState, action) => {
   const newState = merge({}, state);
 
   switch (action.type) {
-    case RECEIVE_DOCUMENT: {
-      // 1: {id: 1, name: 'document name'}
-      const previousDocument = newState[action.document.id];
-      merge(previousDocument, action.document);
+    // case RECEIVE_DOCUMENT:
+    //   // 1: {id: 1, name: 'document name'}
+    //   const previousDocument = newState[action.document.id];
+    //   merge(previousDocument, action.document);
+    //   return newState;
+
+    case RECEIVE_DOCUMENT:
+      merge(newState, { [action.document.id]: action.document });
       return newState;
-    }
+
     case RECEIVE_DOCUMENT_DIFFS: {
       const previousDocument = newState[action.document.id];
-      const newDocument = combineDocumentWithDiffs(previousDocument, action.documentDiffs);
+      const newDocument = combineDocumentWithDiffs(
+        previousDocument,
+        action.documentDiffs
+      );
       merge(previousDocument, newDocument);
       return newState;
     }
+    case REMOVE_DOCUMENT:
+      delete newState[action.documentId];
+      return newState;
+
     default:
       return state;
   }

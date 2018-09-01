0a22f3d5c6502d0dfc2d91ae9839c51e2a01b383
znrm <moradianzaniar@gmail.com>
2018-08-31 18:48:24 -0700

    Complete canvas experiemtns

--- client/canvas_testing/canvas.js
+++ client/canvas_testing/canvas.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ctx = canvas.getContext('2d', { alpha: true });
   const width = window.innerWidth;
   const height = window.innerHeight;
+
   canvas.width = width;
   canvas.height = height;
 
@@ -13,67 +14,60 @@ document.addEventListener('DOMContentLoaded', () => {
   const pointer = { x: 0, y: 0 };
   let pressing = false;
 
-  const boundingBox = (from, to) => {
-    const minX = Math.min(from.x, to.x);
-    const minY = Math.min(from.y, to.y);
-    const maxX = Math.max(from.x, to.x);
-    const maxY = Math.max(from.y, to.y);
+  let previousImage = ctx.getImageData(0, 0, width, height).data;
+  let currentImage;
 
-    const startIndex = 4 * (minY * width + minX);
-    const endIndex = 4 * (maxY * width + maxX);
+  function calculateDiffs() {
+    let thereWasAtLeastOneDiff;
 
-    return [startIndex, endIndex];
-  };
-
-  function calculateDiff(imageBefore, imageAfter, range) {
-    const { length } = imageBefore;
+    currentImage = ctx.getImageData(0, 0, width, height).data;
     const diffs = {};
-    let thereWasAtLeastOneDiff = false;
+    const { length } = currentImage;
 
     for (let i = 0; i < length; i += 1) {
-      thereWasAtLeastOneDiff = true;
-      const diff = imageAfter[i] - imageBefore[i];
+      const diff = currentImage[i] - previousImage[i];
 
       if (diff !== 0) {
-        diffs[i + range[0]] = diff;
+        thereWasAtLeastOneDiff = true;
+        diffs[i] = diff;
       }
     }
-    if (thereWasAtLeastOneDiff) {
-      console.log(diffs);
-    }
+    if (thereWasAtLeastOneDiff) console.log(diffs);
+
+    previousImage = currentImage;
   }
 
-  function draw(from, to) {
-    const range = boundingBox(from, to);
+  canvas.addEventListener(
+    'mousedown',
+    e => {
+      window.setInterval(calculateDiffs, 100);
+      Object.assign(pointer, { x: e.clientX, y: e.clientY });
+      pressing = true;
+    },
+    false
+  );
 
-    const imageBefore = ctx
-      .getImageData(0, 0, width, height)
-      .data.slice(...range);
+  canvas.addEventListener(
+    'mousemove',
+    e => {
+      const currentPointer = { x: e.clientX, y: e.clientY };
+      if (pressing) {
+        ctx.beginPath();
+        ctx.moveTo(pointer.x, pointer.y);
+        ctx.lineTo(currentPointer.x, currentPointer.y);
+        ctx.stroke();
+      }
+      Object.assign(pointer, currentPointer);
+    },
+    false
+  );
 
-    ctx.beginPath();
-    ctx.moveTo(from.x, from.y);
-    ctx.lineTo(to.x, to.y);
-    ctx.stroke();
-
-    const imageAfter = ctx
-      .getImageData(0, 0, width, height)
-      .data.slice(...range);
-
-    calculateDiff(imageBefore, imageAfter, range);
-  }
-
-  document.onmousedown = e => {
-    Object.assign(pointer, { x: e.clientX, y: e.clientY });
-    pressing = true;
-  };
-
-  document.onmousemove = e => {
-    const currentPointer = { x: e.clientX, y: e.clientY };
-    if (pressing) draw(pointer, currentPointer);
-    Object.assign(pointer, currentPointer);
-  };
-
-  document.onmouseup = () => {
-    pressing = false;
-  };
+  canvas.addEventListener(
+    'mouseup',
+    () => {
+      window.clearInterval(calculateDiffs, 500);
+      pressing = false;
+    },
+    true
+  );
 });

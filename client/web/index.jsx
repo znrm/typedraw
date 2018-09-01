c17c15550af550513cb24e28e676e4fa21e5c1fe
znrm <moradianzaniar@gmail.com>
2018-09-01 11:32:18 -0700

    Removing missing dependency from index

--- client/web/index.jsx
+++ client/web/index.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from '../shared/store/store';
 import Root from './components/root';
-import startSocket from '../shared/util/socket_util';
 import { fetchCurrentSession } from '../shared/actions/session_actions';
 
 const axios = require('axios');
 
 document.addEventListener('DOMContentLoaded', () => {
-  const socket = startSocket();
-  socket.emit('working', 'Client is able to emit with socket');
-  socket.on('greeting', res => console.log(res));
-  window.socket = socket;
   const store = configureStore();
   const root = document.getElementById('root');
 

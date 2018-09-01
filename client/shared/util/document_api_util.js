d780fc11a8493c723f69ddf81ec39aa65eafe3af
Aidan Gadberry <agadberr@gmail.com>
2018-08-31 22:13:24 -0700

    Add frontend document actions

--- client/shared/util/document_api_util.js
+++ client/shared/util/document_api_util.js
@@ -1,7 +1,24 @@
 const axios = require('axios');
 
-export const updateText = document =>
-  axios.patch('https://typedraw.herokuapp.com/api/documentTBD/login', document);
+export const updateTitle = (document, title) =>
+  axios.put(`https://typedraw.herokuapp.com/api/documents/${document.id}`, {
+    title
+  });
 
-export const updateImage = image =>
-  axios.patch('https://typedraw.herokuapp.com/api/documentTBD/login', image);
+export const updateText = (document, textLayer) =>
+  axios.put(`https://typedraw.herokuapp.com/api/documents/${document.id}`, {
+    textLayer
+  });
+
+export const updateImage = (document, imageLayer) =>
+  axios.put(`https://typedraw.herokuapp.com/api/documents/${document.id}`, {
+    imageLayer
+  });
+
+export const addCollaborator = (document, user) =>
+  axios.put(`https://typedraw.herokuapp.com/api/documents/${document.id}`, {
+    collaborators: user
+  });
+
+export const deleteDocument = documentId =>
+  axios.delete(`https://typedraw.herokuapp.com/api/documents/${documentId}`);

a794b8f45cdb0386c4d7147839d4d4be8004540b
matt_moe <mrobertmoe@gmail.com>
2018-08-31 19:42:01 -0700

    toggle reducer completed

--- client/shared/reducers/ui_reducer.js
+++ client/shared/reducers/ui_reducer.js
@@ -1,5 +1,5 @@
 import { merge } from 'lodash';
-import { SELECT_DOCUMENT_ACTION, SELECT_DOCUMENT, ERASE_TOGGLE, SELECT_COLOR } from '../actions/ui_actions';
+import { SELECT_DOCUMENT_ACTION, SELECT_DOCUMENT, TOGGLE, SELECT_COLOR } from '../actions/ui_actions';
 
 const initialUI = {
   documentAction: 'typing',
@@ -13,7 +13,7 @@ const initialUI = {
 const uiReducer = (state = initialUI, action) => {
   Object.freeze(state);
   const newState = merge({}, state);
-
+  // added stubs for erase toggle and select color
   switch (action.type) {
     case SELECT_DOCUMENT_ACTION:
       newState.documentAction = action.documentAction;
@@ -21,10 +21,12 @@ const uiReducer = (state = initialUI, action) => {
     case SELECT_DOCUMENT:
       newState.selectedDocument = action.documentId;
       return newState;
-    case ERASE_TOGGLE:
-      return 'blah';
+    case TOGGLE:
+      newState[action.uiElement] = !newState[action.uiElement];
+      return newState;
     case SELECT_COLOR:
-      return 'blahblah';
+      newState.color = action.color;
+      return newState;
     default:
       return state;
   }

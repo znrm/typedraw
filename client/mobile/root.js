db17e848f65dd4ff422177e2f006c5fa621d929a
znrm <moradianzaniar@gmail.com>
2018-09-01 04:39:15 -0700

    Separate navigators from root

--- client/mobile/root.js
+++ client/mobile/root.js
@@ -1,36 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStackNavigator } from 'react-navigation';
-import HomeContainer from './components/home/home_container';
-import Welcome from './components/splash/welcome';
-import Splash from './components/splash/splash';
-import DocumentFrameContainer from './components/documents/document_frame_container';
-import AddCollaboratorsContainer from './components/documents/add_collaborators_container';
-
-const RootStack = createStackNavigator(
-  {
-    Welcome,
-    Splash
-  },
-  { initialRouteName: 'Welcome' }
-);
-
-const AppStack = createStackNavigator(
-  {
-    HomeContainer,
-    DocumentFrameContainer,
-    AddCollaboratorsContainer,
-  },
-  {
-    initialRouteName: 'HomeContainer',
-  },
-
-);
+import { AppStack, RootStack } from './navigation';
 
 const mapState = ({ session }) => ({ loggedIn: session.loggedIn });
 
-const Root = ({ loggedIn }) => (loggedIn ? <AppStack /> : <RootStack />);
+const Root = connect(mapState)(
+  ({ loggedIn }) => (loggedIn ? <AppStack /> : <RootStack />)
+);
 
-const RootContainer = connect(mapState)(Root);
-
-export default RootContainer;
+export default Root;

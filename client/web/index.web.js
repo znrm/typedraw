bd65b2ac38cfabb49439a69baf01dae0b7c35e9d
znrm <moradianzaniar@gmail.com>
2018-09-03 11:36:27 -0700

    Attempt initial build based on react-native-web docs

---
+++ client/web/index.web.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from '../App';
+
+// register the app
+AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', {
+  initialProps: {},
+  rootTag: document.getElementById('root')
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,57 @@
 const path = require('path');
 
+const appDirectory = path.resolve(__dirname, 'client');
+
+const babelLoaderConfiguration = {
+  test: /\.js$/,
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(appDirectory, 'node_modules', 'expo', 'AppEntry.js'),
+    path.resolve(appDirectory, 'shared'),
+    path.resolve(appDirectory, 'mobile'),
+    path.resolve(appDirectory, 'node_modules/react-native'),
+    path.resolve(appDirectory, 'node_modules/react-navigation'),
+    path.resolve(appDirectory, 'node_modules/native-base')
+  ],
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+      // The 'react-native' preset is recommended to match React Native's packager
+      presets: ['react-native'],
+      // Re-write paths to import only the modules needed by the app
+      plugins: ['react-native-web']
+    }
+  }
+};
+
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png|svg)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]'
+    }
+  }
+};
+
 module.exports = {
-  context: __dirname,
-  entry: './client/web/index.jsx',
+  entry: [path.resolve(appDirectory, 'web', 'index.web.js')],
+
   output: {
-    path: path.resolve(__dirname, 'server', 'static'),
-    filename: 'main.js'
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'server', 'static')
   },
+
   resolve: {
-    extensions: ['.js', '.jsx', '*']
+    alias: {
+      'react-native$': 'react-native-web'
+    },
+    extensions: ['.web.js', '.js']
   },
+
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+    rules: [babelLoaderConfiguration, imageLoaderConfiguration]
   },
   devtool: 'source-map'
 };

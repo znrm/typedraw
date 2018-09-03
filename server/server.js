c340b7fc8cdbf555eba6b47220ffe28e7b92263d
znrm <moradianzaniar@gmail.com>
2018-09-02 18:51:38 -0700

    Add image to socket routes; stub image and text handler functions

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
-const socketIO = require('socket.io');
 const path = require('path');
+const startSockets = require('./app/start_sockets');
 const users = require('./routes/api/users');
 const session = require('./routes/api/session');
 const documents = require('./routes/api/documents');
@@ -50,17 +50,4 @@ const port = process.env.PORT || 5000;
 const server = app.listen(port, () =>
   console.log(`Server is running on port ${port}`));
 
-const io = socketIO.listen(server);
-
-io.sockets.on('connection', socket => {
-  socket.on('document', documentId => {
-    socket.join(documentId);
-
-    socket.on('typing', text => {
-      console.log(text);
-      socket.to(documentId).emit('text', text);
-    });
-  });
-});
-
-module.exports = server;
+startSockets(server);

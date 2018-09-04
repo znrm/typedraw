493cee3d777926de90cf5917003100769fa77854
znrm <moradianzaniar@gmail.com>
2018-09-04 12:21:30 -0700

    Add case for documents reducer on start session action

--- client/shared/reducers/documents_reducer.js
+++ client/shared/reducers/documents_reducer.js
@@ -1,59 +1,16 @@
 import { merge } from 'lodash';
+import { START_SESSION } from '../actions/session_actions';
 import {
   RECEIVE_DOCUMENT,
-  RECEIVE_DOCUMENT_DIFFS,
   REMOVE_DOCUMENT
 } from '../actions/document_actions';
-
-// This has not been implemented yet
-// At the moment, it is just returning previousDocument and
-// console logging changes that have been sent from the server
-const combineDocumentWithDiffs = (previousDocument, documentDiffs) => {
-  console.log(documentDiffs);
-  return previousDocument;
-};
 
 const initialState = {
   0: {
     id: 0,
-    textLayer: 'hi',
+    textLayer: 'This is just a default document!',
     imageLayer: [],
-    title: 'Document Zero',
-    owner: 'guest@guest.com'
-  },
-  1: {
-    id: 1,
-    textLayer: 'Hello',
-    imageLayer: [],
-    title: 'Greetings',
-    owner: 'guest@guest.com'
-  },
-  2: {
-    id: 2,
-    textLayer: 'Started something',
-    imageLayer: [],
-    title: 'Snappy Title',
-    owner: 'guest@guest.com'
-  },
-  3: {
-    id: 3,
-    textLayer: 'Started something part 2',
-    imageLayer: [],
-    title: 'Cool Drawings yet to come',
-    owner: 'guest@guest.com'
-  },
-  4: {
-    id: 4,
-    textLayer: 'Started something part 3',
-    imageLayer: [],
-    title: 'Group Collab 3',
-    owner: 'guest@guest.com'
-  },
-  5: {
-    id: 5,
-    textLayer: 'Started something part 4',
-    imageLayer: [],
-    title: 'Project 2',
+    title: 'Default Doc',
     owner: 'guest@guest.com'
   }
 };
@@ -66,15 +23,8 @@ const DocumentReducer = (state = initialState, action) => {
     case RECEIVE_DOCUMENT: {
       return merge(newState, { [action.document.id]: action.document });
     }
-    case RECEIVE_DOCUMENT_DIFFS: {
-      const previousDocument = newState[action.document.id];
-      const newDocument = combineDocumentWithDiffs(
-        previousDocument,
-        action.documentDiffs
-      );
-      merge(previousDocument, newDocument);
-      return newState;
-    }
+    case START_SESSION:
+      return merge(newState, action.res.data.documents);
     case REMOVE_DOCUMENT:
       delete newState[action.documentId];
       return newState;

7c072159960583f9176b16ebd4e72cea0fddacc3
znrm <moradianzaniar@gmail.com>
2018-09-28 06:56:25 -0700

    Complete making splash page the home

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
 const path = require('path');
-const startSockets = require('./app/start_sockets');
+const startSockets = require('./services/start_sockets');
 const users = require('./routes/api/users');
 const session = require('./routes/api/session');
 const documents = require('./routes/api/documents');
-const db = process.env.PROD_MONGODB || require('./config/keys').mongoURI;
+const { mongoURI } = require('./config/keys');
+
+const db = process.env.PROD_MONGODB || mongoURI;
 
 require('./config/passport')(passport);
 
@@ -45,7 +47,6 @@ app.get('/', (req, res) => {
 app.use('/api/users', users);
 app.use('/api/session', session);
 app.use('/api/documents', documents);
-
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Running on port ${port}`));
